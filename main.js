55f00db012a808991a73dcf4a7bfaa9b5074b8c2
hakeem-adjei-git <hakeem@code.iogit config --global --listgit versiongit config --global user.email hakeem@code.io>
2020-10-09 02:22:36 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+makeCoffee(2,10);
+function makeCoffee (sugar, milk)
+{
+    var instructions ="Boil water,";
+    instructions += "pour into a cup,";
+    instructions += "add coffee granules";
+    instructions += "add" + sugar +"spoons of sugar";
+    instrcutions += "add" + milk + "% milk";
+    return instructions;
+}
