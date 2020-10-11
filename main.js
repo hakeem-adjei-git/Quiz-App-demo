1660b179595be13a1b4fa8dff96b3fd9bc0b8ed4
hakeem-adjei-git <hakeem@code.iogit config --global --listgit versiongit config --global user.email hakeem@code.io>
2020-10-11 09:14:54 +0000

    worked on adding event

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 let elements= 
-// document.querySelector('[name="cssProperty"]');
 document.getElementsByName('cssProperty');
 
 let div = document.querySelector('#modify');
 
 function set()
 {
-    for (let index = 0; index < elements.length; index++)
+        for (let index = 0; index < elements.length; index++)
     {
-   let cssProperty = elements[index].getAttribute('id');
+   let cssProperty = elements[ index ].getAttribute('id');
 
     let cssValue = elements[ index ].value;
-
-    console.log(elements[index].value);
     
     div.style[ cssProperty ] = cssValue;
 
     }
    
 }
+
 document.querySelector('#set').addEventListener('click',set);
+ 
